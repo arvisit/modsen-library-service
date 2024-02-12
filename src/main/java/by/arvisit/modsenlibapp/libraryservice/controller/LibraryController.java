1f7fbe4c5115442fa7b264464785253f3db59716
Artem Sitko <sitko.ar@gmail.com>
2024-02-12 17:37:18 +0300

    Remove request dto for book returning

--- src/main/java/by/arvisit/modsenlibapp/libraryservice/controller/LibraryController.java
+++ src/main/java/by/arvisit/modsenlibapp/libraryservice/controller/LibraryController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import by.arvisit.modsenlibapp.libraryservice.dto.BorrowedBookResponseDto;
-import by.arvisit.modsenlibapp.libraryservice.dto.ReturnBookRequestDto;
 import by.arvisit.modsenlibapp.libraryservice.dto.ReturnedBookResponseDto;
 import jakarta.annotation.security.RolesAllowed;
 
@@ -54,8 +52,7 @@ public class LibraryController {
 
     @PatchMapping("/borrowed/{id}/return")
     @RolesAllowed("ADMIN")
-    public ReturnedBookResponseDto returnBook(@PathVariable @UUID String id,
-            @RequestBody ReturnBookRequestDto request) {
+    public ReturnedBookResponseDto returnBook(@PathVariable @UUID String id) {
         // TODO implement!
         return null;
     }
--- src/main/java/by/arvisit/modsenlibapp/libraryservice/dto/ReturnBookRequestDto.java
+++
@@ -1,7 +0,0 @@
-package by.arvisit.modsenlibapp.libraryservice.dto;
-
-import java.time.LocalDate;
-
-public record ReturnBookRequestDto(LocalDate returnedDate) {
-
-}
